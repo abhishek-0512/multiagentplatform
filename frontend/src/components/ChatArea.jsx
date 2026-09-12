cfb64cc7d82bc61df3fcf568fbe8a38925e9987d
Abhishek Gangwar <abhishekgangwar9876@gmail.com>
2026-09-12 17:33:05 +0530

    feat(components): assemble ChatArea view layout with message fetching logic

--- frontend/src/components/ChatArea.jsx
+++ frontend/src/components/ChatArea.jsx
@@ -1,8 +1,40 @@
+import { useEffect } from 'react'
+import Nav from './Nav'
+import MessageList from './MessageList'
+import ChatInput from './ChatInput'
+import { useDispatch, useSelector } from 'react-redux'
+import getMessages from '../features/getMessages'
+import { setArtifacts, setMessages } from '../redux/messageSlice'
+
 function ChatArea() {
+  const {selectedConversation}=useSelector(state=>state.conversation)
+  const dispatch=useDispatch()
+  useEffect(()=>{
+    const getMesg=async () => {
+      if(selectedConversation){
+        if(selectedConversation.title=="New Chat") {
+          dispatch(setMessages([]))
+          dispatch(setArtifacts([]))
+          return;
+        }
+        const data=await getMessages(selectedConversation?._id)
+        const safeData = Array.isArray(data) ? data : []
+        dispatch(setMessages(safeData))
+        const latestArtifactMessage=[...safeData].reverse().find(msg=>msg.artifacts && msg.artifacts.length>0)
+        dispatch(setArtifacts(latestArtifactMessage?.artifacts || []))
+      } else {
+        dispatch(setMessages([]))
+        dispatch(setArtifacts([]))
+      }
+    }
+
+    getMesg()
+  },[dispatch, selectedConversation])
   return (
-    <div>
-      chatarea
-      
+    <div className='flex-1 flex flex-col min-w-0'>
+      <Nav/>
+      <MessageList/>
+      <ChatInput/>
     </div>
   )
 }
